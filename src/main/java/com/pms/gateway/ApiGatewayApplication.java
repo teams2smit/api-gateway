cd8b7e6ccc4173aac62702f1d8984435b27b40d4
Jay Jadhav <jadhav.jay0253@gmail.com>
2022-02-23 15:29:09 +0530

    working fine dont touch the code

--- src/main/java/com/pms/gateway/ApiGatewayApplication.java
+++ src/main/java/com/pms/gateway/ApiGatewayApplication.java
@@ -1,9 +1,18 @@
 package com.pms.gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,6 +20,31 @@ public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator getRoute(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(t -> t.path("/stock/**").uri("lb://medical-stock-service"))
+                .route(t -> t.path("/auth/**").uri("lb://authorization-service"))
+                .route(t -> t.path("/schedule/**").uri("lb://schedule-service"))
+                .route(t -> t.path("/supply/**").uri("lb://supply-service"))
+                .build();
+    }
+    @Bean
+    @Autowired
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+        String ip = null;
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        config.setNonSecurePort(8084);
+        config.setIpAddress(ip);
+        config.setPreferIpAddress(true);
+        return config;
     }
 
 }
